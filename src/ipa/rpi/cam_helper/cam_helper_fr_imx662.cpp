740bb6baeecda63da8e46c42ddecd73d9c9b00b3
Dominik Bajec <d.bajec@framos.com>
2024-11-22 14:18:00 +0100

    rpi:camera_helper: Remove comments
    Signed-off-by: Dominik Bajec <d.bajec@framos.com>

--- src/ipa/rpi/cam_helper/cam_helper_fr_imx662.cpp
+++ src/ipa/rpi/cam_helper/cam_helper_fr_imx662.cpp
@@ -36,56 +36,16 @@ CamHelperimx662::CamHelperimx662()
 {
 }
 
-/**
- * TODO: check if this calculation is correct!
- * 
- * FRAMOS added comment:
- * 	-comment source from libcamera/src/ipa/libipa/camera_sensor_helper.cpp
- * 
- * \brief Compute gain code from the analogue gain absolute value
- * \param[in] gain The real gain to pass
- *
- * This function aims to abstract the calculation of the gain letting the IPA
- * use the real gain for its estimations.
- *
- * \return The gain code to pass to V4L2
- */
 uint32_t CamHelperimx662::gainCode(double gain) const
 {
-	/**
-	 * max gain in dec. value = 240 -> 0xf0 (analog+digital)
-	 * Gain step defined in datasheet is 10/3
-	 */
 	int code = 20 * log10(gain) * 10/3;
 	return std::max(0, std::min(code, 0xf0));
 }
 
-/**
- * TODO: check if this calculation is correct!
- * 
- * FRAMOS added comment:
- * 	-comment source from libcamera/src/ipa/libipa/camera_sensor_helper.cpp
- * 
- * \brief Compute the real gain from the V4L2 subdev control gain code
- * \param[in] gainCode The V4L2 subdev control gain
- *
- * This function aims to abstract the calculation of the gain letting the IPA
- * use the real gain for its estimations. It is the counterpart of the function
- * CameraSensorHelper::gainCode.
- *
- * \return The real gain
- */
 double CamHelperimx662::gain(uint32_t gainCode) const
 {
 
-	/**
-	 * uint32_t CamHelperimx662::gainCode(double gain) const
-	 * will return 158.680749 for gain value 240
-	 * 
-	 * We need to convert it back to 240 value
-	 */
 	return pow(10, 0.015 * gainCode);
-
 }
 
 void CamHelperimx662::getDelays(int &exposureDelay, int &gainDelay,
--- src/ipa/rpi/cam_helper/cam_helper_fr_imx900.cpp
+++ src/ipa/rpi/cam_helper/cam_helper_fr_imx900.cpp
@@ -36,56 +36,16 @@ CamHelperimx900::CamHelperimx900()
 {
 }
 
-/**
- * TODO: check if this calculation is correct!
- * 
- * FRAMOS added comment:
- * 	-comment source from libcamera/src/ipa/libipa/camera_sensor_helper.cpp
- * 
- * \brief Compute gain code from the analogue gain absolute value
- * \param[in] gain The real gain to pass
- *
- * This function aims to abstract the calculation of the gain letting the IPA
- * use the real gain for its estimations.
- *
- * \return The gain code to pass to V4L2
- */
 uint32_t CamHelperimx900::gainCode(double gain) const
 {
-	/**
-	 * max gain in dec. value = 480 -> 0x1E0 (analog+digital)
-	 * Gain step defined in datasheet is 0.1
-	 */
 	int code = 20 * log10(gain) * 10;
 	return std::max(0, std::min(code, 0x1E0));
 }
 
-/**
- * TODO: check if this calculation is correct!
- * 
- * FRAMOS added comment:
- * 	-comment source from libcamera/src/ipa/libipa/camera_sensor_helper.cpp
- * 
- * \brief Compute the real gain from the V4L2 subdev control gain code
- * \param[in] gainCode The V4L2 subdev control gain
- *
- * This function aims to abstract the calculation of the gain letting the IPA
- * use the real gain for its estimations. It is the counterpart of the function
- * CameraSensorHelper::gainCode.
- *
- * \return The real gain
- */
 double CamHelperimx900::gain(uint32_t gainCode) const
 {
 
-	/**
-	 * uint32_t CamHelperimx900::gainCode(double gain) const
-	 * will return 536.24824747 for gain value 480
-	 * 
-	 * We need to convert it back to 480 value
-	 */
 	return pow(10, 0.005 * gainCode);
-
 }
 
 void CamHelperimx900::getDelays(int &exposureDelay, int &gainDelay,
